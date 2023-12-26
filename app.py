a7308aed46fb1551a480918e287bfb747f160318
Akbarshoh1701 <akbarsuvonov5157@gmail.com>
2023-12-26 21:07:27 +0500

    Yangi misol

--- app.py
+++ app.py
@@ -1,3 +1,49 @@
+
+"""1-misol
+
+# class Solution:
+#     def defangIPaddr(self, address: str) -> str:
+#         return address.replace(".", "[.]")
+# # """
+
+
+#2-misol
+"""class Solution:
+    def buildArray(self, nums: list[int]) -> list[int]:
+        new_list = []
+        for i in nums:
+            new_list.append(nums[i])
+
+        return new_list
+"""
+
+#3 mispl
+"""class Solution:
+    def finalValueAfterOperations(self, operations: list[str]) -> int:
+        X = 0
+        for i in operations:
+            if i == "--X" or i == "X--":
+                X -= 1
+            elif i == "X++" or "++X":
+                X += 1
+
+        return X
+
+"""
+
+#4-misol
 class Solution:
-    def defangIPaddr(self, address: str) -> str:
-        return address.replace(".", "[.]")
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        resalt = ""
+        bir, ikki = len(word1), len(word2)
+        nev_list = min(bir, ikki)
+
+        for i in range(nev_list):
+            resalt += word1[1] + word2[i]
+
+        resalt += word1[nev_list:] + word2[nev_list:]
+
+
+
+
+
--- list.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def buildArray(self, nums: list[int]) -> list[int]:
-        new_list = []
-        for i in nums:
-            new_list.append(nums[i])
-
-        return new_list
