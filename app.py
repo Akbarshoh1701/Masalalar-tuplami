5676a758b7bf2ab12c78e6aaa4a736e9b19ef665
Akbarshoh1701 <akbarsuvonov5157@gmail.com>
2024-01-20 20:56:53 +0500

    add misol: 9. Palindrome Number

--- app.py
+++ app.py
@@ -428,3 +428,41 @@ class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
         return len(set(Counter(arr).values()))==len(Counter(arr).values())
 """
+
+
+# misol: 907. Sum of Subarray Minimums
+
+"""
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        n = len(arr)
+        left = [-1] * n 
+        right = [n] * n
+        stack = []
+        for i, value in enumerate(arr):
+            while stack and arr[stack[-1]] >= value:  
+                stack.pop()  
+            if stack:
+                left[i] = stack[-1]  
+            stack.append(i) 
+
+        stack = [] 
+        for i in range(n - 1, -1, -1):  
+            while stack and arr[stack[-1]] > arr[i]: 
+                stack.pop()  
+            if stack:
+                right[i] = stack[-1]  
+            stack.append(i) 
+
+        mod = 10**9 + 7 
+        result = sum((i - left[i]) * (right[i] - i) * value for i, value in enumerate(arr)) % mod
+      
+        return result 
+"""
+
+
+# misol: 9. Palindrome Number
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        son = str(x)
+        return son == son[::-1]
