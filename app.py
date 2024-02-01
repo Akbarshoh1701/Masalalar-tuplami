4f3cdfa72337be45847801cb46e9091192a18b2e
akbar <akbarsuvonov5157@gmail.com>
2024-02-01 20:07:37 +0500

    add now example

--- app.py
+++ app.py
@@ -4,6 +4,7 @@
 #     def defangIPaddr(self, address: str) -> str:
 #         return address.replace(".", "[.]")
 # # """
+from typing import Optional
 
 # 2-misol
 """class Solution:
@@ -576,4 +577,40 @@ class Solution:
 
         return res
                 
+"""
+
+"""
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# class Solution:
+#     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+#         dummy_head = ListNode(0)
+#         current = dummy_head
+#         carry = 0
+# 
+#         while l1 or l2:
+#             val1 = l1.val if l1 else 0
+#             val2 = l2.val if l2 else 0
+#             total = val1 + val2 + carry
+#             carry = total // 10
+#             digit = total % 10
+# 
+#             current.next = ListNode(digit)
+#             current = current.next
+# 
+#             if l1:
+#                 l1 = l1.next
+#             if l2:
+#                 l2 = l2.next
+# 
+#         if carry > 0:
+#             current.next = ListNode(carry)
+# 
+#         return dummy_head.next
+# 
+# 
+# 
 """
