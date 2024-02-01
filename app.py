72f784a7978a06f318d7d67771e8dd6af9bc2479
akbar <akbarsuvonov5157@gmail.com>
2024-02-01 17:33:55 +0500

    feat: add now example

--- app.py
+++ app.py
@@ -528,3 +528,52 @@ solution = Solution()
 print(solution.findPaths(m, n, maxMove, startRow, startColumn))  # Output: 6
 
 """
+#
+#
+# class Solution:
+#     def numberGame(self, nums: list[int]) -> list[int]:
+#         nums.sort()
+#         arr = []
+#         while nums:
+#             elis = nums.pop(0)
+#             bob = nums.pop(0)
+#             arr.append(bob)
+#             arr.append(elis)
+#         return arr
+# class Solution:
+#     def numberGame(self, nums: list[int]) -> list[int]:
+#         nums.sort()
+#         arr = []
+#         while nums:
+#             if len(nums) >= 2:
+#                 elis = nums.pop(0)
+#                 bob = nums.pop(0)
+#                 arr.append(bob)
+#                 arr.append(elis)
+#             else:
+#                 # If there's only one element left, append it to the result
+#                 arr.append(nums.pop(0))
+#         return arr
+
+
+#misol:2966. Divide Array Into Arrays With Max Difference
+"""
+class Solution:
+    def divideArray(self, nums: List[int], k: int) -> List[List[int]]:
+        # Sort the input array
+        nums.sort()
+        res = []
+
+        # Iterate over the sorted array in steps of 3
+        for i in range(0, len(nums), 3):
+            # Check if there are at least 3 elements remaining
+            if i + 2 < len(nums):
+                # If the difference between the maximum and minimum elements in the subarray is greater than k, return an empty list
+                if nums[i + 2] - nums[i] > k:
+                    return []
+                # Append the current subarray to the result
+                res.append([nums[i], nums[i + 1], nums[i + 2]])
+
+        return res
+                
+"""
