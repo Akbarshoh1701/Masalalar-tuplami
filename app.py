b394e6bd38302250f3d0b5b751b23a480d794fb2
akbar <akbarsuvonov5157@gmail.com>
2024-02-02 21:02:57 +0500

    faut:add now exampl

--- app.py
+++ app.py
@@ -614,3 +614,22 @@ class Solution:
 # 
 # 
 """
+"""
+
+#misol 1291. Sequential Digits
+class Solution:
+    def sequentialDigits(self, low, high):
+        c = "123456789"
+        a = []
+
+        for i in range(len(c)):
+            for j in range(i + 1, len(c) + 1):
+                curr = int(c[i:j])
+                if low <= curr <= high:
+                    a.append(curr)
+
+        a.sort()
+        return a
+
+
+"""
